3394f383db4fb453560c41c1eb99b2ef160194d9
Mayank Jain <mayankj2112@gmail.com>
2023-06-24 12:46:25 -0400

    update smart contract to reduce World Coin Auths

--- contracts/MedicalRecord.sol
+++ contracts/MedicalRecord.sol
@@ -4,133 +4,125 @@ contract MedicalRecords {
 
     constructor() {}
 
-    struct record {
-        string id;
-        address patient;
-        int age;
+    struct Record {
+        string worldId;
+        address ownerWallet;
+        string gender;
         string birthday;
-        address[] owners;
         int heightCM;
         int weightKG;
-        string info;
+        address[] contributors;
+        string note;
         string[] images;
     }
 
-    struct person {
-        string id;
-        address adrs;
+    struct Note {
+        address author;
+        string datetime;
+        string content;
     }
 
-    mapping(address => record) private records;
+    mapping(string => Record) private records; // World ID to record
 
-    modifier onlyContributor() {
-        address from = msg.sender;
-        require(isContributor(from), "Caller is not a contributor of the record");
+    // PREREQ CHECKS
+
+    modifier onlyContributor(string memory _worldId) {
+        require(isContributor(_worldId), "Caller is not a contributor of the record");
         _;
     }
 
-    modifier onlyPatient() {
-        address from = msg.sender;
-        require(records[from].patient == from, "Caller is not the patient of the record");
+    modifier onlyOwner(string memory _worldId) {
+        require(records[_worldId].ownerWallet == msg.sender, "Caller is not the record owner");
         _;
     }
 
-    modifier recordExists(address _address) {
-        require(records[_address].patient != address(0), "Record does not exist for the patient");
+    modifier recordExists(string memory _worldId) {
+        require(records[_worldId].ownerWallet != address(0), "Record does not exist for the wallet");
         _;
     }
 
-    function getRecord() public view recordExists(msg.sender) returns (record memory) {
-        address from = msg.sender;
-        return records[from];
+    // WRITE
+    
+
+    function createRecord(
+        string memory _worldId,
+        string memory _gender,
+        string memory _birthday,
+        int _heightCM,
+        int _weightKG
+    ) public {
+        require(records[_worldId].ownerWallet == address(0), "Record already exists for the caller");
+
+        Record memory newRecord;
+
+        newRecord.worldId = _worldId;
+        newRecord.ownerWallet = msg.sender;
+        newRecord.gender = _gender;
+        newRecord.birthday = _birthday;
+        newRecord.heightCM = _heightCM;
+        newRecord.weightKG = _weightKG;
+        newRecord.note = "";
+
+        records[_worldId] = newRecord;
     }
 
-    function isContributor(address _address) public view recordExists(msg.sender) returns (bool) {
-        address from = msg.sender;
-        if(records[from].patient == from) {
+    function modifyRecord(string memory _worldId, string memory _gender, int _heightCM, int _weightKG, string memory _note) public onlyContributor(_worldId) {
+        records[_worldId].gender = _gender;
+        records[_worldId].heightCM = _heightCM;
+        records[_worldId].weightKG = _weightKG;
+        records[_worldId].note = _note;
+    }
+
+    function changeWallet(string memory _worldId, address _newAddress) public onlyOwner(_worldId) {
+        records[_worldId].ownerWallet = _newAddress;
+    }
+
+    // function addNote(string memory _worldId, address _author, string memory _datetime, string memory _content) public onlyContributor(_worldId) {
+
+    //     Note memory newNote;
+    //     newNote.author = _author;
+    //     newNote.datetime = _datetime;
+    //     newNote.content = _content;
+
+    //     records[_worldId].notes.push(newNote);
+    // }
+
+    function setContributors(string memory _worldId, address[] memory _contributors) public onlyOwner(_worldId) {
+        records[_worldId].contributors = _contributors;
+    }
+
+    function addImage(string memory _worldId, string memory _image) public onlyContributor(_worldId) {
+        records[_worldId].images.push(_image);
+    }
+
+    // READ
+
+    function hasRecord(string memory _worldId) public view returns (bool) {
+        return records[_worldId].ownerWallet != address(0);
+    }
+
+    function isOwner(string memory _worldId) public view recordExists(_worldId) returns (bool) {
+        if(records[_worldId].ownerWallet == msg.sender) {
             return true;
         }
-        for (uint256 i = 0; i < records[from].owners.length; i++) {
-            if (records[from].owners[i] == _address) {
+        return false;
+    }
+
+    function isContributor(string memory _worldId) public view recordExists(_worldId) returns (bool) {
+        if (isOwner(_worldId)) {
+            return true;
+        }
+
+        for (uint256 i = 0; i < records[_worldId].contributors.length; i++) {
+            if (records[_worldId].contributors[i] == msg.sender) {
                 return true;
             }
         }
         return false;
     }
 
-    function createRecord(
-    string memory uuid,
-    int age,
-    string memory birthday,
-    int heightCM,
-    int weightKG
-    ) public {
-        address from = msg.sender;
-        require(records[from].patient == address(0), "Record already exists for the caller");
-
-        record memory newRecord;
-        newRecord.id = uuid; // Set the provided UUID as the ID
-        newRecord.patient = from; // Set the patient as the caller's address
-        newRecord.age = age; // Initialize the age
-        newRecord.birthday = birthday; // Initialize the birthday
-        newRecord.heightCM = heightCM; // Initialize the height
-        newRecord.weightKG = weightKG; // Initialize the weight
-
-        records[from] = newRecord;
+    function getRecord(string memory _worldId) public view onlyContributor(_worldId) returns (Record memory) {
+        return records[_worldId];
     }
 
-    function updateAge(int256 newAge) public onlyContributor recordExists(msg.sender) {
-        address from = msg.sender;
-        records[from].age = newAge;
-    }
-
-    function updateBirthday(string memory newBirthday) public onlyContributor recordExists(msg.sender) {
-        address from = msg.sender;
-        records[from].birthday = newBirthday;
-    }
-
-    function updateHeight(int256 newHeightCM) public onlyContributor recordExists(msg.sender) {
-        address from = msg.sender;
-        records[from].heightCM = newHeightCM;
-    }
-
-    function updateWeight(int256 newWeightKG) public onlyContributor recordExists(msg.sender) {
-        address from = msg.sender;
-        records[from].weightKG = newWeightKG;
-    }
-
-    function updateInfo(string memory newInfo) public onlyContributor recordExists(msg.sender) {
-        address from = msg.sender;
-        records[from].info = newInfo;
-    }
-
-    function addContributor(address contributor) public onlyPatient {
-        address from = msg.sender;
-        records[from].owners.push(contributor);
-    }
-    
-    function removeContributor(address contributor) public onlyPatient {
-        address from = msg.sender;
-        uint length = records[from].owners.length;
-        
-        for (uint i = 0; i < length; i++) {
-            if (records[from].owners[i] == contributor) {
-                if (i != length - 1) {
-                    records[from].owners[i] = records[from].owners[length - 1];
-                }
-                records[from].owners.pop();
-                break;
-            }
-        }
-    }
-    
-    function addImage(string memory image) public onlyContributor recordExists(msg.sender) {
-        address from = msg.sender;
-        records[from].images.push(image);
-    }
-
-    function hasRecord() public view returns (bool) {
-        address from = msg.sender;
-        return records[from].patient != address(0);
-    }
 }
