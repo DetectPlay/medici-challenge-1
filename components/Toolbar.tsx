07deceb6dfd43616a13ba371d7ff94c0bec723d2
Mayank Jain <mayankj2112@gmail.com>
2023-06-25 04:37:30 -0400

    Dynamic Toolbar

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -10,44 +10,62 @@ import {
 import React, { useEffect, useState } from "react";
 import IDKitLoginButton from "./IDKitLoginButton";
 import Link from "next/link";
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useContract, useContractRead } from "@thirdweb-dev/react";
 import { useRouter } from "next/router";
 
 export default function Toolbar() {
 	const [userId, setUserId] = useState<null | string>();
+	const [hasRecord, setHasRecord] = useState<boolean | null>(null);
+	const { contract } = useContract("0xB4fDd313A8E59891BE02A34242A3d0Bb2Ef113B2");
 
 	useEffect(() => {
 		setUserId(localStorage.getItem("userId"));
 	}, []);
 
+	useEffect(() => {
+		if (contract && userId) {
+			const checkRecord = async () => {
+				return await contract.call("hasRecord", [userId]);
+			};
+			checkRecord().then(setHasRecord);
+		}
+	}, [contract, userId]);
+
 	const router = useRouter();
 	return (
 		<Flex h={69} align={"center"} bg="teal" px={25}>
-			<ButtonGroup>
-				<Button onClick={() => router.push("/create")}>Create</Button>
-				<Button
-					onClick={() =>
-						router.push({
-							pathname: "/update",
-							query: { userId: userId },
-						})
-					}
-				>
-					Update
-				</Button>
+			{hasRecord !== null && (
+				<ButtonGroup>
+					{!hasRecord ? (
+						<Button onClick={() => router.push("/create")}>Create</Button>
+					) : (
+						<>
+							<Button
+								onClick={() =>
+									router.push({
+										pathname: "/update",
+										query: { userId: userId },
+									})
+								}
+							>
+								Update
+							</Button>
+							<Button
+								onClick={() =>
+									router.push({
+										pathname: "/contributors",
+										query: { userId: userId },
+									})
+								}
+							>
+								Contributors
+							</Button>
 
-				<Button
-					onClick={() =>
-						router.push({
-							pathname: "/contributors",
-							query: { userId: userId },
-						})
-					}
-				>
-					Contributors
-				</Button>
-				<Button onClick={() => router.push("/scans")}>Scans</Button>
-			</ButtonGroup>
+							<Button onClick={() => router.push("/scans")}>Scans</Button>
+						</>
+					)}
+				</ButtonGroup>
+			)}
 			<Spacer />
 			<ConnectWallet />
 			<Box>
